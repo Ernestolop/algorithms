5e34bdddd7dc185b80ae22719a7a9d0b42da512e
Ernestolop <ernestodaniellopez504@gmail.com>
2025-03-02 14:02:16 -0300

    Add readme and logs for insertion sort

--- sort/insertionSort/InsertionSort.java
+++ sort/insertionSort/InsertionSort.java
@@ -1,7 +1,13 @@
+package insertionSort;
+
+import java.util.Arrays;
+
 class InsertionSort {
     public static void main(String[] args) {
         int[] listToSort = { 5, 2, 4, 6, 1, 3 };
+        System.out.println("Before sorting: " + Arrays.toString(listToSort));
         insertionSort(listToSort);
+        System.out.println("After sorting: " + Arrays.toString(listToSort));
     }
 
     private static void insertionSort(int[] listToSort) {
